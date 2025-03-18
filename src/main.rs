bf522d5d8282ba26730f98875d7f78f86d8d6887
Lucas Rack <lucas.rack@lambdaclass.com>
2025-03-18 15:38:55 -0300

    Add GameOfLife struct and fmt fn

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ pub mod model;
 use model::game_of_life::GameOfLife;
 
 fn main() {
-    let  _gol = GameOfLife{};
-    println!("Hello, world!");
+    let  gol = GameOfLife::new();
+
+     println!("{gol}");
 }
--- src/model/game_of_life.rs
+++ src/model/game_of_life.rs
@@ -1,4 +1,48 @@
+use std::fmt; 
+
+
+enum LifeStatus{
+    Alive,
+    Dead
+}
 
 pub struct GameOfLife{
+    data: Vec<Vec<LifeStatus>>,
+    step: u32
+}
 
+impl GameOfLife{
+    pub fn new() -> Self {
+        GameOfLife{
+            data: vec!(
+                vec!(LifeStatus::Alive, LifeStatus::Dead,LifeStatus::Alive ),
+                vec!(LifeStatus::Dead, LifeStatus::Alive,LifeStatus::Dead ),
+                vec!(LifeStatus::Alive, LifeStatus::Dead,LifeStatus::Alive ),
+            ),
+            step: 0
+        }
+    }
+
+    pub fn data_as_str(&self) -> String {
+        let mut data_as_str: String = String::new();
+        for line in &self.data {
+            for v in line {
+                data_as_str.push(
+                    match v {
+                        LifeStatus::Alive => '⬜',
+                        LifeStatus::Dead => '⬛'
+                    }
+                );
+            }
+            data_as_str.push('\n');
+        }
+        data_as_str
+    }
+}
+
+impl fmt::Display for GameOfLife {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        
+        write!(f, "{}\n\nSTEP: {}",self.data_as_str(), self.step)
+    }
 }
