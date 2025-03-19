4ac9f0f062bf3c9a5474a2fcfde0ac7b0b9e715b
Lucas Rack <lucas.rack@lambdaclass.com>
2025-03-19 14:53:19 -0300

    remove loop from inner fn

--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,15 @@ use view::screen::Screen;
 #[macroquad::main("Conway's game of life")]
 async fn main() {
     let mut screen = Screen::new();
-    // screen.main_loop().await
 
-    loop{
+    loop {
         let _ = screen.check_buttons();
-        screen.draw_frame(vec!(
-            vec!('1','0','0','0','1'),
-            vec!('0','1','0','0','1'),
-            vec!('0','1','1','0','0'),
-        )).await
+        screen
+            .draw_frame(vec![
+                vec!['1', '0', '0', '0', '1'],
+                vec!['0', '1', '0', '0', '1'],
+                vec!['0', '1', '1', '0', '0'],
+            ])
+            .await
     }
 }
