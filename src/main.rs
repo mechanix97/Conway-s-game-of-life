4bdfaab5b0417d5e6ea3fa3b452c4b4a5e9e9ba8
Lucas Rack <lucas.rack@lambdaclass.com>
2025-03-19 17:25:00 -0300

    Add thread in the game loop for better performance

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,40 @@
 pub mod model;
 pub mod view;
 
+use std::sync::{Arc, RwLock};
+use std::thread;
+
+
 use model::game_of_life::GameOfLife;
 use view::screen::Screen;
+
+
 
 #[macroquad::main("Conway's game of life")]
 async fn main() {
     let mut screen = Screen::new();
     screen.set_area(-10, -10,110 , 110);
-    let mut gol = GameOfLife::new();
-    gol.randomize(Some(100), Some(100));
+    let mut gol = Arc::new(RwLock::new(GameOfLife::new()));
+    gol.write().unwrap().randomize(Some(100), Some(100));
+
+
+    let mut running = true;
+
+    let mut gol_clone = gol.clone();
+    let mut join = thread::spawn(move || {
+       while running{
+            {gol_clone.write().unwrap().step();}
+            gol_clone.read().unwrap().step_delay(100);
+       }
+    });
 
     loop {
         let _ = screen.check_buttons();
         let area = screen.get_area();
-        screen.draw_frame(gol.data_as_vec(area)).await;
-        gol.step();
-        gol.step_delay(100);
+        let data;
+        {data = gol.read().unwrap().data_as_vec(area);}
+        screen.draw_frame(data).await;
     }
+
+    running = false;
 }
